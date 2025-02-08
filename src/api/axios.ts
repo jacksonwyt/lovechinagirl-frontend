fc61331f7d63b30ea02ba346e46713c627fb1573
Ty Westbrook <tyjacksonwestbrook@gmail.com>
2025-02-08 13:51:05 -0800

    api calls

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { toast } from 'react-hot-toast';
 
 const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   withCredentials: true
 });
 
