16191d971f4d6205cc680f04c3a22cb2febef501
Ty Westbrook <tyjacksonwestbrook@gmail.com>
2025-02-08 10:15:17 -0800

    image config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import { NextConfig } from 'next'
 
 const config: NextConfig = {
   images: {
-    domains: ['localhost'],
+    domains: ['lovechinagirldesign-assets.s3.us-west-1.amazonaws.com'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/webp'],
